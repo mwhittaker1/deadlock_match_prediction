632597d036a7b297feedb99735bb1064eb8787a5
mwhittaker1 <mickeywhittaker@gmail.com>
2025-04-15 22:37:55 -0400

    starting building player_hero functions

--- Deadlock_Player_Hero_Data/main.py
+++ Deadlock_Player_Hero_Data/main.py
@@ -1,6 +1,73 @@
+import pandas as pd
+import requests
+import logging
+import time
+from datetime import timedelta
 
 
+# logging setup
+def setup_logging(verbose: bool):
+    level = logging.DEBUG if verbose else logging.INFO
+
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler("script.log"),
+            logging.StreamHandler()
+        ]
+    )
+    logging.debug("This is a debug message")
+    logging.info("This is an info message")
+    logging.warning("This is a warning")
+    logging.error("This is an error")
+    logging.critical("This is critical")
+
+setup_logging(verbose=True)
+logger = logging.getLogger(__name__)
+logger.debug("Debug mode on")
+
+### Fetch hero, match, account data, calculate win/loss (w/l) % as well as 1-2 month trends of data
 #Fetch hero data
+def get_m_hero_data():
+    #API connection information
+    site = "https://api.deadlock-api.com"
+    endpoint = "/v1/analytics/hero-stats?" 
+
+    min_average_badge = "/min_average_badge=115" #Minimum average match player rank
+    c_unix_timestamp = int(time.time()) #current time
+    x_days = 1    #days back to fetch
+    x_days_ago = str(c_unix_timestamp - timedelta(days=1).total_seconds())    
+    min_unix_time = f"/min_unix_timestamp={x_days_ago}"
+    print(min_unix_time)
+    url = site+endpoint+min_average_badge+min_unix_time
+    logging.debug(f"Getting data from full url: {url}")
+
+    #get json
+    response = requests.get(url)
+    if response.status_code == 200:
+        m_hero_data = response
+    else:
+        logging.error(f"Error fetching data, code = {response.status_code}")
+
+    #return json
+    return m_hero_data
+
+def main():
+    logging.info(f"Starting scrpt...")
+    logging.info(f"Fetchin match_hero data...")
+    raw_m_h_data = get_m_hero_data()
+    logging.info(f"match_hero data fetched.")
+
+    logging.debug(f"")
+    with open("output.json", "w") as f:
+        f.write(raw_m_h_data)
+
+    return
+
+if __name__ == main():
+    main()
+
 ## Hero pickrate for high ranked games over past 7 days
 
 ## Hero w/l for high ranked games over past month
