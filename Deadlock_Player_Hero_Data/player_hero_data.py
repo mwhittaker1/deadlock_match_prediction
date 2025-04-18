db412d30ae626d6c04af8fd62699e39e517d9110
mwhittaker1 <mickeywhittaker@gmail.com>
2025-04-18 00:27:20 -0400

    working get account_id_hero_data and account_id_specific_hero filtering.

--- Deadlock_Player_Hero_Data/player_hero_data.py
+++ Deadlock_Player_Hero_Data/player_hero_data.py
@@ -25,12 +25,12 @@ setup_logging(verbose=True)
 logger = logging.getLogger(__name__)
 logger.debug("Debug mode on")
 
-## Fetch player data from player_id
+## Fetch player data from account_id, collects all hero data for player, filters for h_id, then returns player_hero data for h_id hero.
 def get_player_hero_data(p_id,h_id):
 
     # fetch player_hero from API on player_id
     site = "https://api.deadlock-api.com"
-    endpoint = f"v1/players/{p_id}/hero-stats'"
+    endpoint = f"/v1/players/{p_id}/hero-stats"
     url = site+endpoint
     
     logging.info(f"sending request for response")
@@ -43,46 +43,54 @@ def get_player_hero_data(p_id,h_id):
     else:
         print(f"Failed to retrieve match data: {response.status_code}")
     
-    p_h_data = next((item for item in p_all_h_data if item == h_id), None)
+    p_h_data = next((item for item in p_all_h_data if item['hero_id'] == h_id), None)
     
     if p_all_h_data:
-        logging.info(f"found player_hero data for hero: {h_id} printing data \n {p_h_data}")
+        logging.info(f"\n\nfound player_hero data for hero: {h_id} printing data \n {p_h_data}")
     else:
         logging.error(f"get_player_hero_data, find h_id in player data did not find match")
 
-    return p_h_data #Returns JSON of match data.
+    return p_h_data #.json of h_id for p_id
+
+# for each account_id, hero_id in df, pull player_hero data.
+def cycle_account_ids(df):
+    i=1
+    for index,row in df.iterrows(): #For each account_id in test_data
+        p_id = row['account_id']
+        h_id = row['hero_id']
+        logging.info(f"\n\ngetting player data....p_id is {p_id} and h_id is {h_id} interation {i}")
+        p_hero_data = get_player_hero_data(p_id, h_id)
+        
+        print(f"player hero data is: {p_hero_data}")
+
+        filtered_p_hero_data = filter_player_hero_data(p_hero_data) # function not written yet
+        final_p_hero_data = calculate_player_hero_stats(filtered_p_hero_data) # function not written yet
+        #append final_p_hero_data to row['account_id']
+        i+=1
+        return p_hero_data
 
 def main():
     logging.info(f"Starting... reading .csv")
     test_data = pd.read_excel(f'single_account_data.xlsx')
+    logging.info(f"test_data is now {test_data}")
     i=0
-    for index in test_data:
-        logging.info(f"iteration through player_ids, count {i}")
-        p_id = index['player_id']
-        h_id = index['hero_id']
-        logging.info(f"getting player data....")
-        p_hero_data = get_player_hero_data(p_id, h_id)
-        print(f"player hero data is: {p_hero_data}")
-        i+=1
+    p_hero_data = cycle_account_ids(test_data) #will need to become a different variable later-
 
 if __name__ == main():
     main()
 
-def player_hero_format(df):
+def filter_player_hero_data(df):
     ## p_hero total games played as p_hero_total_games (int)
     ## p_hero w/l over last 1 months as p_hero_1m_wl (int)
     ## p_hero w/l over last week as p_hero_1w_wl (int)
 
     return
 
-def get_player_stats():
+def calculate_player_hero_stats():
     ## player w/l over last 3 games as player_3g_wl (int)
     ## player w/l over last month as player_1m_wl (int)
     return
 
-def format_player_stats(p_id):
-    # formats player data received
-    return
 
 
 
