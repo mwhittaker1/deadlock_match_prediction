892f5e8f2a20d6719637226cc63622ab4bcfc113
mwhittaker1 <mickeywhittaker@gmail.com>
2025-08-17 13:44:24 -0400

    create differential data processing, standard deviation statistical method, and support functions to run either configuration. Currently 4 models, same parameters have run.

---
+++ data/fetch_data.py
@@ -0,0 +1,14 @@
+
+# imports
+
+""" About this script
+This script fetches data from various sources and prepares it for analysis.
+"""
+
+# logging
+
+# fetch active matches for prediction
+
+# fetch specific set of matches for analysis, training supplement, or prediction of a specific period
+
+# fetch historical matches for training
