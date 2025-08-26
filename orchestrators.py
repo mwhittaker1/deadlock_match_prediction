80b27f63348702b6dd0547bbedd487e033591f79
mwhittaker1 <mickeywhittaker@gmail.com>
2025-08-26 12:00:06 -0400

    working on match<>player_stats merge. handled differently in different processes.

--- orchestrators.py
+++ orchestrators.py
@@ -49,18 +49,35 @@ def create_training_data(start_date,end_date,name="test"):
     hero_stats = hero_stats.add_prefix('h_')
     hero_stats = hero_stats.rename(columns={'h_hero_id': 'hero_id'})
 
-    p_stats = fd.process_player_stats(player_hero_stats)
+    player_stats = fd.process_player_stats(player_hero_stats)
 
     raw_players.to_csv(f"{folder_name}/raw_players.csv", index=False)
     raw_matches.to_csv(f"{folder_name}/raw_matches.csv", index=False)
 
     player_matches.to_csv(f"{folder_name}/player_matches.csv", index=False)
-    p_stats.to_csv(f"{folder_name}/p_stats.csv", index=False)
+    player_stats.to_csv(f"{folder_name}/p_stats.csv", index=False)
     player_hero_stats.to_csv(f"{folder_name}/player_hero_stats.csv", index=False)
     hero_stats.to_csv(f"{folder_name}/h_stats.csv", index=False)
 
+    logging.info(f"Checking unique naming for hero, player_hero, and player stats")
 
+    player_hero_stats, player_stats, hero_stats = dp.check_unique_naming(player_hero_stats, p_stats, hero_stats)
 
+    logging.info(f"Merging stats into player_player_hero_hero_stats (p_ph_h_stats)")
+
+    p_ph_h_stats = dp.merge_player_hero_stats(player_hero_stats, player_stats, hero_stats)
+
+    logging.info(f"Merging completed, saving as p_ph_h_stats.csv to {folder_name}")
+    p_ph_h_stats.to_csv(f"{folder_name}/p_ph_h_stats.csv", index=False)
+
+    logging.info(f"calculating all stats")
+    all_stats = dp.calculate_ph_stats(p_ph_h_stats)
+
+    logging.info(f"Calculations completed, saving all stats to {folder_name}")
+    all_stats.to_csv(f"{folder_name}/all_stats.csv", index=False)
+
+    logging.info(f"Preparing match data to be merged with stats")
+    player_match_stats = dp.prepare_match_stats(raw_matches, player_stats)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
